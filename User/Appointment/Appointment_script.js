514b48bc4518f4411c82f0c594bcf4cfe62dd190
Adthep Kawe a <0931939341youn@gmail.com>
2025-02-26 21:55:06 +0700

    111

--- User/Appointment/Appointment_script.js
+++ User/Appointment/Appointment_script.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const bookingInfo = JSON.parse(localStorage.getItem('bookingInfo')) || {};
 
     // Set the values of the appointment details
+    document.getElementById('username').textContent = bookingInfo.username || 'N/A';
     document.getElementById('clinicName').textContent = bookingInfo.clinicName || 'N/A';
     document.getElementById('reason').textContent = bookingInfo.reason || 'N/A';
     document.getElementById('date').textContent = bookingInfo.date || 'N/A';
