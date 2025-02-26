de565cf97e8218969b22a5031b35c5221c692bb1
Adthep Kawe a <0931939341youn@gmail.com>
2025-02-27 06:32:32 +0700

    แสดงprofile บุคคลทั่วไป

--- User/profile_บุคคลทั่วไป/Profile.js
+++ User/profile_บุคคลทั่วไป/Profile.js
@@ -12,3 +12,26 @@ function toggleMenu() {
         dropdownMenu.classList.toggle("show");
     }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var userName = localStorage.getItem('username');
+    var userEmail = localStorage.getItem('userEmail');
+    var userNumber = localStorage.getItem('userNumber');
+
+    var usernameElement = document.getElementById("username");
+    var userNameElement = document.getElementById("userName");
+    var emailElement = document.getElementById("email");
+    var userNumberElement = document.getElementById("userNumber");
+
+    if (usernameElement && userName) {
+        usernameElement.textContent = userName;
+    }
+    if (userNameElement && userName) {
+        userNameElement.textContent = userName;
+    }
+    if (emailElement && userEmail) {
+        emailElement.textContent = `อีเมล : ${userEmail}`;
+    }
+    if (userNumberElement && userNumber) {
+        userNumberElement.textContent = `เบอร์โทรศัพท์ : ${userNumber}`;
+    }
+});
